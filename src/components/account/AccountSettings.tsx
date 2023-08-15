32422ccf59cdae8e87b807939e438308038f3e85
Niwaiwa <po2846@gmail.com>
2023-08-16 02:20:14 +0800

    feat: add account setting update action

--- src/components/account/AccountSettings.tsx
+++ src/components/account/AccountSettings.tsx
@@ -1,17 +1,61 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { TextField, Typography, Box, Button } from '@mui/material';
+import axios from 'axios';
+import { RootState } from '../../globalRedux/store';
+import { setUserInfo } from '../../globalRedux/features/auth/authSlice';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
+
+
+interface CustomState extends SnackbarOrigin {
+    open: boolean;
+    message?: string;
+}
 
 const AccountSettings: React.FC = () => {
 
+  const [snackBarState, setSnackBarState] = useState<CustomState>({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal, open: snackBarOpen, message } = snackBarState;
+
   const [nickname, setNickname] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const token = useSelector((state: RootState) => state.auth.token);
+  const dispatch = useDispatch();
 
   const handleUploadNickname = () => {
-    console.log('Nickname uploaded');
+    if (nickname === '') return;
+    sendUserUpdate({
+      'nickname': nickname,
+    });
   };
 
   const handleUploadPassword = () => {
-    console.log('Password uploaded');
+    if (password === '') return;
+    sendUserUpdate({
+      'password': password,
+    });
+  };
+
+  const sendUserUpdate = async (data: object) => {
+    try {
+      let loginData = {...data};
+      const header = {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${token}`,
+      }
+
+      const serverURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+      const response = await axios.put(`${serverURL}/api/users/`, loginData, { headers: header });
+      const userInfo = response.data;
+      dispatch(setUserInfo(userInfo));
+      setSnackBarState({ ...snackBarState, open: true, message: 'Update success' });
+    } catch (error) {
+      setSnackBarState({ ...snackBarState, open: true, message: 'Update failed' });
+    }
   };
 
   return (
@@ -67,6 +111,14 @@ const AccountSettings: React.FC = () => {
           </form>
         </Box>
       </Box>
+      <Snackbar
+        anchorOrigin={{ vertical: vertical, horizontal: horizontal }}
+        autoHideDuration={3000}
+        open={snackBarOpen}
+        onClose={() => setSnackBarState({ ...snackBarState, open: false })}
+        message={message}
+        key={vertical + horizontal}
+      />
     </Box>
   );
 };
