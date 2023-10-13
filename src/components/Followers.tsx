6921eea6575e30abe2290fbe7ce7b7a021a916a5
Niwaiwa <po2846@gmail.com>
2023-10-13 22:55:02 +0800

    feat: add pagination and add loading progress

--- src/components/Followers.tsx
+++ src/components/Followers.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, Pagination } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
@@ -9,11 +10,12 @@ import AvatarUser from './AvatarUser';
 
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
-const useFollowers = (userId: string | null) => {
+const useFollowers = (userId: string | null, page: number | null) => {
   const serverURL = process.env.NEXT_PUBLIC_BACKEND_URL;
   const url = `${serverURL}/api/users/${userId}/follower`;
+  const newUrl = page !== null && page !== undefined && page > 1 ? `${url}?page=${page}` : url;
   const { data, error } = useSWR( 
-    !userId ? null : { url: url },
+    !userId ? null : { url: newUrl },
     key => fetcher(key.url),
     { 
       revalidateOnFocus: false, 
@@ -38,51 +40,68 @@ const Followers: React.FC<FollowersProps> = (props) => {
   // const router = useRouter();
   const userId = props.userId ? props.userId : '';
   // const username = props.username ? props.username : '';
+  const [followerPage, setFollowerPage] = useState(1);
 
-  const { followerData, followerIsLoading, followerIsError } = useFollowers(userId);
+  const { followerData, followerIsLoading, followerIsError } = useFollowers(userId, followerPage);
+  const followers = followerData?.data ? followerData.data : [];
   const followerCount = followerData?.count ? followerData.count : 0;
 
-  const followers = followerData?.data ? followerData.data : [];
-  const [followerPage, setFollowerPage] = useState(1);
-  const [followingCount, setFollowingCount] = useState(0);
+  const handleFollowerPageChange = async (event: React.ChangeEvent<unknown>, value: number) => {
+    setFollowerPage(value);
+  }
 
   return (
     <Box>
-      <Typography variant="h6">Followers</Typography>
-      <Box sx={{ marginTop: '15px' }}>
-        { followerIsError ?
-          <Typography variant="body2">{`Error! Please try again.`}</Typography> 
-          :
-          followers?.length === 0 ?
-          <Typography variant="body2">{`You don't have any followers.`}</Typography>
-          :
-          <Grid 
-            container
-            wrap={'wrap'}
-            sx={{
-              marginRight: '-15px',
-              marginLeft: '-15px',
-              backgroundColor: '#f5f5f5',
-            }}
-          >
-            {followers?.map((data: any) => (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                key={data.id}
-                sx={{ 
-                  paddingRight: '15px',
-                  paddingLeft: '15px'
+      { followerIsError ?
+        <Typography variant="body2">{`Error! Please try again.`}</Typography> 
+        :
+        followerIsLoading ?
+        <CircularProgress />
+        :
+        followers?.length === 0 ?
+        <Typography variant="body2">{`You don't have any followers.`}</Typography>
+        :
+        <>
+          <Typography variant="h6">Followers</Typography>
+          <Box sx={{ marginTop: '15px' }}>
+              <Grid 
+                container
+                wrap={'wrap'}
+                sx={{
+                  marginRight: '-15px',
+                  marginLeft: '-15px',
+                  backgroundColor: '#f5f5f5',
                 }}
               >
-                <AvatarUser user={data} />
+                {followers?.map((data: any) => (
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    key={data.username}
+                    sx={{ 
+                      paddingRight: '15px',
+                      paddingLeft: '15px'
+                    }}
+                  >
+                    <AvatarUser user={data} />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-        }
-      </Box>
+          </Box>
+          <Box display={'flex'} justifyContent={'center'} sx={{ marginTop: '10px' }}>
+            {followerCount <= 30 ? null :
+              <Pagination 
+                count={Math.ceil(followerCount / 30)} 
+                defaultPage={1}
+                page={followerPage} 
+                onChange={(e, page) => handleFollowerPageChange(e, page)}
+              />
+            }
+          </Box>
+        </>
+      }
     </Box>
   );
 };
--- src/components/Followings.tsx
+++ src/components/Followings.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, Pagination } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
@@ -9,16 +10,17 @@ import AvatarUser from './AvatarUser';
 
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
-const useFollowings = (userId: string | null) => {
+const useFollowings = (userId: string | null, page: number | null) => {
   const serverURL = process.env.NEXT_PUBLIC_BACKEND_URL;
   const url = `${serverURL}/api/users/${userId}/following`;
+  const newUrl = page !== null && page !== undefined && page > 1 ? `${url}?page=${page}` : url;
   const { data, error } = useSWR( 
-    !userId ? null : { url: url },
+    !userId ? null : { url: newUrl },
     key => fetcher(key.url),
     { 
       revalidateOnFocus: false, 
       revalidateOnReconnect: false,
-      // refreshInterval: 0,
+      refreshInterval: 0,
     }
   );
   return {
@@ -38,51 +40,68 @@ const Followings: React.FC<FollowingsProps> = (props) => {
   // const router = useRouter();
   const userId = props.userId ? props.userId : '';
   // const username = props.username ? props.username : '';
+  const [followingPage, setFollowingPage] = useState(1);
 
-  const { followingData, followingIsLoading, followingIsError } = useFollowings(userId);
+  const { followingData, followingIsLoading, followingIsError } = useFollowings(userId, followingPage);
+  const followings = followingData?.data ? followingData.data : [];
   const followingCount = followingData?.count ? followingData.count : 0;
 
-  const followings = followingData?.data ? followingData.data : [];
-  const [followingsPage, setFollowerPage] = useState(1);
-  const [followingsCount, setFollowingCount] = useState(0);
+  const handleFolloingPageChange = async (event: React.ChangeEvent<unknown>, value: number) => {
+    setFollowingPage(value);
+  }
 
   return (
     <Box>
-      <Typography variant="h6">Followings</Typography>
-      <Box sx={{ marginTop: '15px' }}>
-        { followingIsError ?
-          <Typography variant="body2">{`Error! Please try again.`}</Typography> 
-          :
-          followings?.length === 0 ?
-          <Typography variant="body2">{`You don't have any followings.`}</Typography>
-          :
-          <Grid 
-            container
-            wrap={'wrap'}
-            sx={{
-              marginRight: '-15px',
-              marginLeft: '-15px',
-              backgroundColor: '#f5f5f5',
-            }}
-          >
-            {followings?.map((data: any) => (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                key={data.id}
-                sx={{ 
-                  paddingRight: '15px',
-                  paddingLeft: '15px'
-                }}
-              >
-                <AvatarUser user={data} />
-              </Grid>
-            ))}
-          </Grid>
-        }
-      </Box>
+      { followingIsError ?
+      <Typography variant="body2">{`Error! Please try again.`}</Typography> 
+      :
+      followingIsLoading ?
+      <CircularProgress />
+      :
+      followings?.length === 0 ?
+      <Typography variant="body2">{`You don't have any followings.`}</Typography>
+      :
+      <>
+        <Typography variant="h6">Followings</Typography>
+        <Box sx={{ marginTop: '15px' }}>
+            <Grid 
+              container
+              wrap={'wrap'}
+              sx={{
+                marginRight: '-15px',
+                marginLeft: '-15px',
+                backgroundColor: '#f5f5f5',
+              }}
+            >
+              {followings?.map((data: any) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  key={data.username}
+                  sx={{ 
+                    paddingRight: '15px',
+                    paddingLeft: '15px'
+                  }}
+                >
+                  <AvatarUser user={data} />
+                </Grid>
+              ))}
+            </Grid>
+        </Box>
+        <Box display={'flex'} justifyContent={'center'} sx={{ marginTop: '10px' }}>
+            {followingCount <= 30 ? null :
+              <Pagination 
+                count={Math.ceil(followingCount / 30)} 
+                defaultPage={1}
+                page={followingPage} 
+                onChange={(e, page) => handleFolloingPageChange(e, page)}
+              />
+            }
+          </Box>
+      </>
+      }
     </Box>
   );
 };
